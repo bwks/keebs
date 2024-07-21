8767d7b4e25678636c3e26336cc20a66ce502720
Brad Searle <bradleysearle@gmail.com>
2024-07-21 19:38:06 +1000

    v3 keybindings

--- dactyl_v3/main.py
+++ dactyl_v3/main.py
@@ -89,7 +89,7 @@ keyboard.keymap = [
         KC.TAB,      KC.Q,        KC.W,            KC.E,           KC.R,         KC.T,                                            KC.Y,         KC.U,        KC.I,          KC.O,         KC.P,           KC.BSLS,
         KC.MO(1),    KC.A,        KC.S,            KC.D,           KC.F,         KC.G,                                            KC.H,         KC.J,        KC.K,          KC.L,         KC.SCLN,        KC.QUOTE,
         KC.MO(2),    KC.Z,        KC.X,            KC.C,           KC.V,         KC.B,                                            KC.N,         KC.M,        KC.COMM,       KC.DOT,       KC.SLSH,        XXXXX,
-                                  XXXXX,           XXXXX,          KC.LCTRL,     KC.LSHIFT,      KC.LCTRL,         KC.LALT,       KC.ENTER,     KC.SPACE,    XXXXX,         XXXXX,
+                                  XXXXX,           XXXXX,          KC.LCTRL,     KC.LSHIFT,      KC.LCTRL,         KC.LSHIFT,     KC.ENTER,     KC.SPACE,    XXXXX,         XXXXX,
                                                                                  KC.LALT,        KC.LGUI,          KC.LGUI,       KC.ESC,
     ],
     [   # Layer 1
@@ -97,7 +97,7 @@ keyboard.keymap = [
         XXXXX,       VIM_QUIT,    VIM_WR_QUIT,     KC.ESC,         VIM_RAGE_QUIT, XXXXX,                                          KC.MINUS,       KC.EQUAL,    KC.UP,         KC.LBRACKET,    KC.RBRACKET,    XXXXX,
         XXXXX,       XXXXX,       VIM_WR,          XXXXX,          MEH,           XXXXX,                                          HASH_ROCKET,    KC.LEFT,     KC.DOWN,       KC.RIGHT,       DOUBLE_COLON,   WALRUS,
         XXXXX,       XXXXX,       NVIM_CLOSE,      XXXXX,          MEHS,          XXXXX,                                          HASH_BRACKET,   XXXXX,       LEFT_ARROW,    RIGHT_ARROW,    XXXXX,          KC.DF(0),
-                                  XXXXX,           XXXXX,          KC.LCTRL,      KC.LSHIFT,      KC.LCTRL,        KC.LALT,       KC.ENTER,       KC.SPACE,    KC.F11,        KC.F12,
+                                  XXXXX,           XXXXX,          KC.LCTRL,      KC.LSHIFT,      KC.LCTRL,        KC.LSHIFT,     KC.ENTER,       KC.SPACE,    KC.F11,        KC.F12,
                                                                                   KC.LALT,        KC.LGUI,         KC.LGUI,       KC.LALT,
     ],
     [   # Layer 2
@@ -105,7 +105,7 @@ keyboard.keymap = [
         XXXXX,       XXXXX,       XXXXX,           XXXXX,          XXXXX,         XXXXX,                                          XXXXX,          XXXXX,       KC.PGUP,       XXXXX,          XXXXX,          XXXXX,
         XXXXX,       XXXXX,       XXXXX,           XXXXX,          XXXXX,         XXXXX,                                          XXXXX,          KC.HOME,     KC.PGDN,       KC.END,         XXXXX,          XXXXX,
         XXXXX,       XXXXX,       XXXXX,           XXXXX,          XXXXX,         XXXXX,                                          XXXXX,          XXXXX,       XXXXX,         XXXXX,          XXXXX,          KC.DF(0),
-                                  XXXXX,           XXXXX,          KC.LCTRL,      KC.LSHIFT,       KC.LCTRL,       KC.LALT,       XXXXX,          XXXXX,       XXXXX,         XXXXX,          
+                                  XXXXX,           XXXXX,          KC.LCTRL,      KC.LSHIFT,       KC.LCTRL,       KC.LSHIFT,     XXXXX,          XXXXX,       XXXXX,         XXXXX,          
                                                                                   KC.LALT,         XXXXX,          KC.LGUI,       XXXXX,
     ],
 
