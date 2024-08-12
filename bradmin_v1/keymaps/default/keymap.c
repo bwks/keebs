a3ac78de38612f803026737f60e43b243ac8b84f
Brad Searle <bradleysearle@gmail.com>
2024-08-13 05:53:16 +1000

    enter key positions

--- bradmin_v1/keymaps/default/keymap.c
+++ bradmin_v1/keymaps/default/keymap.c
@@ -133,9 +133,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    XXXXXXX, /* <L R> */  XXXXXXX, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, 
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    XXXXXXX, /* <L R> */  XXXXXXX, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, 
       MO(LR1), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    XXXXXXX, /* <L R> */  XXXXXXX, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, 
-      MO(LR2), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    XXXXXXX, /* <L R> */  XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_NO,  
-      XXXXXXX, XXXXXXX, KC_NO,   KC_NO,   KC_LCTL, KC_LSFT, KC_LCTL, /* <L R> */  KC_LSFT, KC_ENT,  KC_SPC,  KC_NO,   KC_NO,   XXXXXXX, XXXXXXX,
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LALT, KC_LGUI, /* <L R> */  KC_LGUI, KC_ESC,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+      MO(LR2), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    XXXXXXX, /* <L R> */  XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,  
+      XXXXXXX, XXXXXXX, KC_NO,   KC_NO,   KC_LCTL, KC_LSFT, KC_LCTL, /* <L R> */  KC_LSFT, KC_ESC,  KC_SPC,  KC_NO,   KC_NO,   XXXXXXX, XXXXXXX,
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LALT, KC_LGUI, /* <L R> */  KC_LGUI, KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
     /*=======, =======, =======, =======, =======, =======, =======, -- <L R> --  =======, =======, =======, =======, =======, =======, =======,*/
     ),
 
@@ -144,9 +144,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   XXXXXXX, /* <L R> */  XXXXXXX, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL, 
       XXXXXXX, VIM_QIT, VIM_WRQ, KC_ESC,  VIM_RQT, KC_NO,   XXXXXXX, /* <L R> */  XXXXXXX, KC_MINS, KC_EQL,  KC_UP,   KC_LBRC, KC_RBRC, KC_NO, 
       XXXXXXX, KC_NO,   VIM_WR,  KC_NO,   KC_MEH,  KC_NO,   XXXXXXX, /* <L R> */  XXXXXXX, HSH_RKT, KC_LEFT, KC_DOWN, KC_RGHT, DBL_CLN, WALRUS, 
-      XXXXXXX, KC_NO,   NVM_QIT, KC_NO,   KC_HYPR, KC_NO,   XXXXXXX, /* <L R> */  XXXXXXX, HSH_BRK, KC_NO,   LFT_ARW, RGT_ARW, KC_NO,   DF(LR0),  
-      XXXXXXX, XXXXXXX, KC_NO,   KC_NO,   KC_LCTL, KC_LSFT, KC_LCTL, /* <L R> */  KC_LSFT, KC_ENT,  KC_SPC,  KC_F11,  KC_F12,  XXXXXXX, XXXXXXX,
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LALT, KC_LGUI, /* <L R> */  KC_LGUI, KC_ESC,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+      XXXXXXX, KC_NO,   NVM_QIT, KC_NO,   KC_HYPR, KC_NO,   XXXXXXX, /* <L R> */  XXXXXXX, HSH_BRK, KC_NO,   LFT_ARW, RGT_ARW, KC_NO,   KC_ENT,  
+      XXXXXXX, XXXXXXX, KC_NO,   KC_NO,   KC_LCTL, KC_LSFT, KC_LCTL, /* <L R> */  KC_LSFT, KC_ESC,  KC_SPC,  KC_F11,  KC_F12,  XXXXXXX, XXXXXXX,
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LALT, KC_LGUI, /* <L R> */  KC_LGUI, KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
     /*=======, =======, =======, =======, =======, =======, =======, -- <L R> --  =======, =======, =======, =======, =======, =======, =======,*/
     ),
 
@@ -155,9 +155,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       XXXXXXX, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   XXXXXXX, /* <L R> */  XXXXXXX, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, 
       XXXXXXX, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   XXXXXXX, /* <L R> */  XXXXXXX, KC_NO,   KC_NO,   KC_PGUP, KC_NO,   KC_PSCR, KC_INS, 
       XXXXXXX, KC_NO,   KC_NO,   KC_NO,   KC_MEH,  KC_NO,   XXXXXXX, /* <L R> */  XXXXXXX, KC_NO,   KC_HOME, KC_PGDN, KC_END,  KC_NO,   KC_NO, 
-      XXXXXXX, KC_NO,   KC_NO,   KC_NO,   KC_HYPR, KC_NO,   XXXXXXX, /* <L R> */  XXXXXXX, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   DF(LR0),  
-      XXXXXXX, XXXXXXX, KC_NO,   KC_NO,   KC_LCTL, KC_LSFT, KC_LCTL, /* <L R> */  KC_LSFT, KC_ENT,  KC_SPC,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LALT, KC_LGUI, /* <L R> */  KC_LGUI, KC_ESC,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+      XXXXXXX, KC_NO,   KC_NO,   KC_NO,   KC_HYPR, KC_NO,   XXXXXXX, /* <L R> */  XXXXXXX, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_ENT,  
+      XXXXXXX, XXXXXXX, KC_NO,   KC_NO,   KC_LCTL, KC_LSFT, KC_LCTL, /* <L R> */  KC_LSFT, KC_ESC,  KC_SPC,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LALT, KC_LGUI, /* <L R> */  KC_LGUI, KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
     /*=======, =======, =======, =======, =======, =======, =======, -- <L R> --  =======, =======, =======, =======, =======, =======, =======,*/
     ),
     
